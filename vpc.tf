7d5397b7e7fd98d9822ad64ae6a3ae88207e42d9
Chandra Shekhar Saini <chandrashekharsaini322@gmail.com>
2026-09-07 18:08:31 +0530

    Fix typo in S3 bucket name

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "chandra-saini-terraform-bucket-btbb5434"
+  bucket = "chandra-saini-terrmmmaform-bucket-btbb5434"
 
   tags = {
     Name        = "Terraform S3 Bucket"
